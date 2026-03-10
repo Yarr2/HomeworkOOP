44d58baec06b2ed6b0a3adea55f520169fcf5cc5
Yarr2 <yarema.stus@gmail.com>
2026-03-10 19:24:05 +0200

    Added CompanyManager.cs

---
+++ CompanyManager.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeworkOOP;
+
+public class CompanyManager
+{
+    private List<Flight> _flights = new List<Flight>();
+    private Dictionary<int, Passanger> _passangers = new Dictionary<int, Passanger>();
+    private Queue<Passanger> _boarding = new Queue<Passanger>();
+    private Stack<Flight> _history = new Stack<Flight>();
+
+    public void AddFlight(Flight flight)
+    {
+        _flights.Add(flight);
+    }
+    
+}
